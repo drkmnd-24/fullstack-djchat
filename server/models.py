f7138ce71e0a015ae621183b396c338cba9a9505
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-11-02 21:00:38 +0800

    djchat start project

---
+++ server/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='server_owner')
