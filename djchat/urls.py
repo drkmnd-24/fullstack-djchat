8b07fd86839d053fc575a576451654da69909a73
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-11-02 21:57:48 +0800

    create swagger docs

--- djchat/urls.py
+++ djchat/urls.py
@@ -16,7 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from server.views import ServerListViewSet
+
+router = DefaultRouter()
+router.register('api/server/select', ServerListViewSet)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="DJ Chatbot",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + router.urls
--- server/views.py
+++ server/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Server
+
+
+class ServerListViewSet(viewsets.ViewSet):
+    queryset = Server.objects.all()
+
+    def list(self, request):
+        category = request.query_params.get('category')
+
+        if category:
+            self.queryset.filter(category=category)
